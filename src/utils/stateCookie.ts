fb82f7b275bbd1658fa570ab2a873709b2c95f59
Alvin Ng <243186+alvinsj@users.noreply.github.com>
2024-06-09 12:44:48 +0800

    moved

--- src/utils/stateCookie.ts
+++ src/utils/stateCookie.ts
@@ -1,19 +1,20 @@
 
-import { saveCookie, getCookie, deleteCookie } from './cookie'
+import { saveCookie, getCookie, deleteCookie } from '@lib/cookie'
+import config from '@/config'
 
 export const createStateCookie = (value: string) => {
   if (!value) throw new Error('state value is required')
 
   const state = crypto.randomUUID()
-  saveCookie(`app.txs.${state}`, value, 30)
+  saveCookie(`${config.STATE_COOKIE_PREFIX}${state}`, value, 30)
 
   return state
 }
 
 export const getStateCookie = (state: string) => {
-  return getCookie(`app.txs.${state}`)
+  return getCookie(`${config.STATE_COOKIE_PREFIX}${state}`)
 }
 
 export const deleteStateCookie = (state: string) => {
-  deleteCookie(`app.txs.${state}`)
+  deleteCookie(`${config.STATE_COOKIE_PREFIX}${state}`)
 }
