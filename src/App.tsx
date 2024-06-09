5c8f45c3ce94afcf7275c1b97f8f07cdb0bcd328
Alvin Ng <243186+alvinsj@users.noreply.github.com>
2024-06-10 01:48:17 +0800

    use AuthStage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react'
 
+import AuthContext from '@/contexts/AuthContext'
 import { getPKCEStatus } from '@/utils/auth'
 import { deleteStateCookie } from '@/utils/stateCookie'
-import AuthContext from '@/contexts/AuthContext'
+import { AuthStage } from '@/types'
 
 import LoginButton from '@/components/LoginButton'
 
@@ -11,67 +12,71 @@ import useGetAccessToken from '@/hooks/useGetAccessToken'
 
 import s from './App.module.css'
 import LogoutButton from './components/LogoutButton'
+import { getAuthStage } from './utils/authStage'
+
+function clearSearchParams() {
+  const url = new URL(window.location.href) // Create a URL object from the current window location
+  url.search = '' // Clear the search parameters
+  window.history.replaceState({}, document.title, url.toString()) // Update the URL without reloading the page
+}
 
 function App() {
   const params = new URLSearchParams(window.location.search)
   const state = params.get('state')
   const code = params.get('code')
-
-
   const { codeVerifier } = getPKCEStatus(state)
-
   const {
     isLoading, error, tokens,
     getATWithAuthCode, getATWithRefreshToken
   } = useGetAccessToken()
+
   const authContext = useAuthContextValue(tokens)
 
+  const authStage = getAuthStage({
+    state, code, codeVerifier,
+    accessToken: authContext.accessToken,
+    refreshToken: authContext.refreshToken,
+  })
+
   useEffect(() => {
-    if (authContext.refreshToken && !authContext.accessToken) {
-      getATWithRefreshToken(authContext.refreshToken)
-    } else if (code && codeVerifier) {
-      getATWithAuthCode(code, codeVerifier)
+    if (authStage.stage === AuthStage.AFTER_AUTH_CODE) {
+      deleteStateCookie(authStage.state)
+      clearSearchParams()
+    } else if (authStage.stage === AuthStage.BEFORE_REFRESH_TOKEN) {
+      getATWithRefreshToken(authStage.refreshToken)
+    } else if (authStage.stage === AuthStage.BEFORE_AUTH_CODE) {
+      getATWithAuthCode(authStage.state, authStage.code, authStage.codeVerifier)
     }
     // once on mount only
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [authStage.stage])
 
-  const justDoneAuthCodeRequest =
-    state && codeVerifier && tokens?.accessToken && tokens?.refreshToken
-  useEffect(() => {
-    if (justDoneAuthCodeRequest) {
-      deleteStateCookie(state)
-      window.history.replaceState({}, document.title, '/')
-    }
-  }, [justDoneAuthCodeRequest, state])
-
-  const status = state && code && codeVerifier ? [
-    `state: ${state}`,
-    `code: ${code}`,
-    `codeVerifier: ${codeVerifier}`
-  ] : []
-
-  const isLoggedIn = !!authContext.accessToken
+  const statuses = {
+    accessToken: authContext.accessToken,
+    refreshToken: authContext.refreshToken,
+    state,
+    code,
+    codeVerifier,
+  }
 
   return (
     <AuthContext.Provider value={authContext}>
       <main className={s['app']}>
-        {isLoggedIn
+        {authStage.stage === AuthStage.LOGGED_IN
           ? <LogoutButton className={s['app-loginBtn']} />
           : <LoginButton className={s['app-loginBtn']} />
         }
+        <h1>Stage: {authStage.stage}</h1>
         <pre>
           {!isLoading && <>
             <table>
               <tbody>
-                <tr>
-                  <td className='debug-itemName'>accessToken</td>
-                  <td className="debug-longText">{`${authContext.accessToken}`}</td>
-                </tr>
-                <tr>
-                  <td className='debug-itemName'>refreshToken</td>
-                  <td className="debug-longText">{`${authContext.refreshToken}`}</td>
-                </tr>
+                {Object.entries(statuses).filter(([, v]) => !!v).map(([key, value]) => (
+                  <tr key={key}>
+                    <td className='debug-itemName'>{key}</td>
+                    <td className="debug-longText">{`${value}`}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </>
