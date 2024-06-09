be7ceb727ca662f8c92c5408d50eee424dccf8e0
Alvin Ng <243186+alvinsj@users.noreply.github.com>
2024-06-09 18:36:36 +0800

    add zod schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { z } from "zod"
+
 export type PKCEStatus = {
   isDone: boolean,
   codeVerifier?: string,
@@ -7,3 +9,9 @@ export type AuthTokens = {
   accessToken: string | null,
   refreshToken: string | null
 }
+
+export const PostTokenSchema = z.object({
+  access_token: z.string(),
+  expires_at: z.number(),
+  refresh_token: z.string(),
+})
